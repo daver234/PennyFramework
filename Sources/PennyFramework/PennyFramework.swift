0a37ac5e790e50fd32ca142ad053e2439a709233
Dave Rothschild <drothschild@bellus3d.com>
2020-05-25 18:08:26 -0700

    - Added documentation

--- Sources/PennyFramework/PennyFramework.swift
+++ Sources/PennyFramework/PennyFramework.swift
@@ -7,6 +7,10 @@ public struct PennyFramework {
     @State var placeholder: String
     @Binding var text: String
     
+    /// This is a test field that is consistent with the design
+    /// - Parameters:
+    ///   - placeholder: text that shows before text entry
+    ///   - text: Binding for the text that gets typed into the field
     public init(placeholder: String, text: Binding<String>) {
       self._placeholder = State(initialValue: placeholder)
       self._text = text
